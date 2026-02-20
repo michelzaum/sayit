d52af726909b6e5bf72d4d2b1330b6f8fc5f9f3d
michelzaum <michel.oliveira999@hotmail.com>
2026-02-19 23:05:48 -0300

    chore: graphql setup and tests

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>,
 );
