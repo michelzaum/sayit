f5cd20d3c568ed1ddfb980806357bf7271d360c9
michelzaum <michel.oliveira999@hotmail.com>
2026-02-20 22:07:22 -0300

    refactor: remove GraphQL test code

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
   cache: new InMemoryCache(),
 });
-
-client
-  .query({
-    query: gql`
-      query GetLocations {
-        locations {
-          id
-          name
-          description
-          photo
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
 
 createRoot(document.getElementById("root")!).render(
   <ApolloProvider client={client}>
