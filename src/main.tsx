c18ddcb6d12661787110a84fc13e344f51d4929a
michelzaum <michel.oliveira999@hotmail.com>
2026-02-20 21:58:05 -0300

    chore: setup tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 
