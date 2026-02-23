c2cd76353c38db2164e402c603454484b5dc35d5
michelzaum <michel.oliveira999@hotmail.com>
2026-02-22 21:42:40 -0300

    feat: create SignUp page

---
+++ src/pages/sign-up/index.tsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router";
+
+export function SignUp() {
+  return (
+    <div className="flex flex-col p-6 gap-10 mt-10">
+      <div className="flex flex-col gap-2">
+        <span className="text-2xl font-medium">Faça o cadastro</span>
+        <span className="text-base text-gray-500">
+          E fique por dentro das ultimas postagens.
+        </span>
+      </div>
+
+      <form className="flex flex-col gap-8">
+        <div className="flex flex-col gap-3">
+          <label htmlFor="name">Primeiro nome</label>
+          <input
+            className="border border-gray-300 rounded-lg h-14 p-3"
+            name="name"
+            id="name"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="email">E-mail</label>
+          <input
+            className="border border-gray-300 rounded-lg h-14 p-3"
+            type="email"
+            name="email"
+            id="email"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="password">Senha</label>
+          <input
+            className="border border-gray-300 rounded-lg h-14 p-3"
+            type="password"
+            name="password"
+            id="password"
+          />
+        </div>
+
+        <button
+          type="button"
+          className="h-14 rounded-lg bg-blue-950 text-gray-50 hover:bg-blue-900 transition-colors cursor-pointer w-full"
+        >
+          Cadastrar
+        </button>
+      </form>
+
+      <div className="flex items-center gap-2 justify-center">
+        <span className="font-medium text-base">Já possui conta?</span>
+        <Link to="/sign-in" className="text-base text-blue-800">
+          Faça login
+        </Link>
+      </div>
+    </div>
+  );
+}
