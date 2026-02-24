d40c96b3d05fd4e5ef5fe7d94701f7dac91d8fdf
michelzaum <michel.oliveira999@hotmail.com>
2026-02-23 22:58:46 -0300

    feat: create apollo server instance and typeDefs and resolvers with Query schema

---
+++ server/index.ts
@@ -0,0 +1,62 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type User {
+    userName: String
+    email: String
+    password: String
+    createdAt: String
+  }
+
+  type Post {
+    content: String
+    author: User
+    likes: [Like!]
+    comments: [Comment!]
+    createdAt: String
+  }
+
+  type Like {
+    userName: String
+    createdAt: String
+  }
+
+  type Comment {
+    body: String
+    userName: String
+    createdAt: String
+  }
+
+  type Query {
+    getUser: User
+    getPosts: [Post!]
+    getPost: Post
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getUser: async () => ({
+      userName: "michelzaum",
+      email: "michel@gmail.com",
+      password: "1234",
+      createdAt: "2026/02/23",
+    }),
+    getPosts: () => [],
+    getPost: () => {},
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
+});
+
+console.log(`Server ready at: ${url}`);
