c8a778184fce21cc253ada3fb1fb0e757844c335
michelzaum <michel.oliveira999@hotmail.com>
2026-03-01 18:50:23 -0300

    feat: create an Interface for ApolloServer inicialization

--- server/src/main/server.ts
+++ server/src/main/server.ts
@@ -2,9 +2,15 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { container } from "./container";
 
+interface IApolloServerContext {
+  // TODO: revisit this to not return any
+  createUserUseCase: any;
+  getUserUseCase: any;
+}
+
 import { typeDefs, resolvers } from "../index";
 
-const server = new ApolloServer({
+const server = new ApolloServer<IApolloServerContext>({
   typeDefs,
   resolvers,
 });
