46205ee96e77dcbdaffd37e36d233266e5bb5598
michelzaum <michel.oliveira999@hotmail.com>
2026-03-02 22:05:39 -0300

    feat: create post type definitions

---
+++ server/src/interface/graphql/schema/post.schema.ts
@@ -0,0 +1,24 @@
+export const postTypeDefs = `#graphql
+  type Post {
+    content: String
+    author: User
+    likes: [Like!]
+    comments: [Comment!]
+    createdAt: String
+  }
+
+  input PostInput {
+    content: String
+    author: String
+  }
+
+  extend type Query {
+    getPosts: [Post!]
+    getPost: Post
+  }
+
+  extend type Mutation {
+    createPost(body: PostInput!): Post
+    updatePost(id: String, body: PostInput): Post
+  }
+`;
