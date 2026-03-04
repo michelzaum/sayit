ea081ad6923d88b7db06383044e60cadab55f654
michelzaum <michel.oliveira999@hotmail.com>
2026-03-04 08:23:44 -0300

    feat: add getUserByEmailUseCase in container context

--- server/src/main/container.ts
+++ server/src/main/container.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from "../modules/user/repositories/UserRepository";
 import { CreateuserUseCase } from "../modules/user/useCases/createUser/CreateUserUseCase";
 import { GetUserUseCase } from "../modules/user/useCases/getUser/GetUserUseCase";
+import { GetUserByEmailUseCase } from "../modules/user/useCases/getUserByEmail/GetUserByEmailUseCase";
 import { IContainer } from "./model";
 
 const userRepository = new UserRepository();
@@ -8,4 +9,5 @@ const userRepository = new UserRepository();
 export const container: IContainer = {
   createUserUseCase: new CreateuserUseCase(userRepository),
   getUserUseCase: new GetUserUseCase(userRepository),
+  getUserByEmailUseCase: new GetUserByEmailUseCase(userRepository),
 };
--- server/src/main/model.ts
+++ server/src/main/model.ts
@@ -1,7 +1,9 @@
 import { CreateuserUseCase } from "../modules/user/useCases/createUser/CreateUserUseCase";
 import { GetUserUseCase } from "../modules/user/useCases/getUser/GetUserUseCase";
+import { GetUserByEmailUseCase } from "../modules/user/useCases/getUserByEmail/GetUserByEmailUseCase";
 
 export interface IContainer {
   createUserUseCase: CreateuserUseCase;
   getUserUseCase: GetUserUseCase;
+  getUserByEmailUseCase: GetUserByEmailUseCase;
 }
