597684ff7b035f900471b32f6fd1982cc2e9c938
michelzaum <michel.oliveira999@hotmail.com>
2026-03-09 08:35:18 -0300

    feat: create 'Post' entity

---
+++ server/src/modules/post/entities/Post.ts
@@ -0,0 +1,11 @@
+import { Comment } from "@/modules/comments/entities/Comment";
+import { Like } from "@/modules/like/entities/Like";
+import { User } from "@/modules/user/entities/User";
+
+export type Post = {
+  content: string;
+  author: User;
+  likes: Like[];
+  comments: Comment[];
+  createdAt: string;
+};
