68412a722738bc831713cca6a6ffbdcfa436e3af
michelzaum <michel.oliveira999@hotmail.com>
2026-03-12 08:03:55 -0300

    feat: call CREATE_POST mutation to create a post through the front-end

--- web/src/components/create-post/useCreatePost.ts
+++ web/src/components/create-post/useCreatePost.ts
@@ -1,14 +1,24 @@
 import { FormEvent, useRef } from "react";
+import { useMutation } from "@apollo/client/react";
+
+import { CREATE_POST } from "./mutation";
 
 export function useCreatePost() {
   const postContentRef = useRef<HTMLTextAreaElement>({} as HTMLTextAreaElement);
+  const [createPost] = useMutation(CREATE_POST);
 
   async function onCreatePostSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const postContentValue = postContentRef.current.value;
 
-    console.log(postContentValue);
+    await createPost({
+      variables: {
+        body: {
+          content: postContentValue,
+        },
+      },
+    });
   }
 
   return {
