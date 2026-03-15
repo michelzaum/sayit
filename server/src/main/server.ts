4868de807897fd9b6ee40db7892702866d82e965
michelzaum <michel.oliveira999@hotmail.com>
2026-03-15 20:31:33 -0300

    feat: add expressMiddleware plugin

--- server/src/main/server.ts
+++ server/src/main/server.ts
@@ -1,5 +1,12 @@
+import http from "http";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@as-integrations/express5";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const httpServer = http.createServer(app);
 
 import { typeDefs } from "../interface/graphql/schema";
 import { resolvers } from "../interface/graphql/resolvers";
@@ -9,25 +16,34 @@ import { IContainer } from "./model";
 const server = new ApolloServer<IContainer>({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: {
-    port: 4000,
-  },
-  context: async ({ req, res }) => {
-    return {
-      http: {
-        req,
-        res,
-      },
-      createUserUseCase: container.createUserUseCase,
-      getUserUseCase: container.getUserUseCase,
-      signInUseCase: container.signInUseCase,
-      createPostUseCase: container.createPostUseCase,
-      listPostsUseCase: container.listPostsUseCase,
-    };
-  },
-});
+await server.start();
 
-console.log(`Server ready at: ${url}`);
+app.use(
+  "graphql",
+  cors<cors.CorsRequest>(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req, res }) => {
+      return {
+        http: {
+          req,
+          res,
+        },
+        createUserUseCase: container.createUserUseCase,
+        getUserUseCase: container.getUserUseCase,
+        signInUseCase: container.signInUseCase,
+        createPostUseCase: container.createPostUseCase,
+        listPostsUseCase: container.listPostsUseCase,
+      };
+    },
+  }),
+);
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve),
+);
+
+console.log("Server ready at http://localhost:4000/graphql");
