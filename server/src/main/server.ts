7451f5627bc0d74b027d5468188b8817cb564fdc
michelzaum <michel.oliveira999@hotmail.com>
2026-03-18 08:11:06 -0300

    feat: update localhost url in console when server is started

--- server/src/main/server.ts
+++ server/src/main/server.ts
@@ -68,4 +68,4 @@ await new Promise<void>((resolve) =>
   httpServer.listen({ port: 4000 }, resolve),
 );
 
-console.log("Server ready at http://localhost:4000/graphql");
+console.log("Server ready at http://localhost:4000");
