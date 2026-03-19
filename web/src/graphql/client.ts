429839507730074cc1ee4b36f5b3382c2faea1aa
michelzaum <michel.oliveira999@hotmail.com>
2026-03-19 08:11:13 -0300

    feat: update graphql client to accept subscriptions via WebSockets operations

--- web/src/graphql/client.ts
+++ web/src/graphql/client.ts
@@ -1,9 +1,32 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { OperationTypeNode } from "graphql";
+import { createClient } from "graphql-ws";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloLink,
+} from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/",
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000/",
+  }),
+);
+
+const splitLink = ApolloLink.split(
+  ({ operationType }) => {
+    return operationType === OperationTypeNode.SUBSCRIPTION;
+  },
+  wsLink,
+  httpLink,
+);
 
 export const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000/",
-    credentials: "include",
-  }),
+  link: splitLink,
   cache: new InMemoryCache(),
 });
