13fd6fcb9bb7cd3fa8ef8bea6d5d3bb0881b7cb2
michelzaum <michel.oliveira999@hotmail.com>
2026-03-22 15:11:35 -0300

    feat: create types.ts file

--- web/src/pages/feed/useFeed.ts
+++ web/src/pages/feed/useFeed.ts
@@ -3,28 +3,8 @@ import { useQuery } from "@apollo/client/react";
 import { toast } from "sonner";
 
 import { GET_POSTS } from "./query";
-import { PostCard } from "../../entities/PostCard";
 import { POST_CREATED_SUBSCRIPTION } from "./subscription";
-
-type GetPosts = {
-  getPosts: PostCard[];
-};
-
-type Post = {
-  id: string;
-  content: string;
-  createdAt: string;
-  likes: { authorId: string }[];
-  comments: { authorId: string }[];
-  author: {
-    name: string;
-    email: string;
-  };
-};
-
-type PostCreatedSubscription = {
-  postCreated: Post;
-};
+import { GetPosts, PostCreatedSubscription } from "./types";
 
 export function useFeed() {
   const { error, loading, data, subscribeToMore } =
