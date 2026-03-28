3c857cc4888b52a576df84217ab7b03af3a66ca6
michelzaum <michel.oliveira999@hotmail.com>
2026-03-28 16:48:54 -0300

    feat: implement 'getPost' query resolver

--- server/src/interface/graphql/resolvers/post/post.query.ts
+++ server/src/interface/graphql/resolvers/post/post.query.ts
@@ -1,7 +1,18 @@
 import { IContainer } from "@/main/model";
 
+interface GetPostArgs {
+  postId: string;
+}
+
 export const postQuery = {
   getPosts: async (_, args, { listPostsUseCase }: IContainer) => {
     return await listPostsUseCase.execute();
+  },
+  getPost: async (
+    _,
+    { postId }: GetPostArgs,
+    { getPostUseCase }: IContainer,
+  ) => {
+    return await getPostUseCase.execute(postId);
   },
 };
