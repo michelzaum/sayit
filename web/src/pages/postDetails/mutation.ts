631baadf84a4bb7e75fefa684249d4318cecc6f8
michelzaum <michel.oliveira999@hotmail.com>
2026-03-30 08:10:53 -0300

    feat: remove 'authorId' argument from CREATE_COMMENT mutation

--- web/src/pages/postDetails/mutation.ts
+++ web/src/pages/postDetails/mutation.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_COMMENT = gql`
-  mutation CreateComment($authorId: String, $postId: String, $content: String) {
-    createComment(authorId: $authorId, postId: $postId, content: $content) {
+  mutation CreateComment($postId: String, $content: String) {
+    createComment(postId: $postId, content: $content) {
       id
       content
       authorName
