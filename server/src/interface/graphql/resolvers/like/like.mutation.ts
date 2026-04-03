5578d0828394c45ad30a739a43ac018ceac251e6
michelzaum <michel.oliveira999@hotmail.com>
2026-04-03 14:21:21 -0300

    feat: create 'deleteLike' mutation

--- server/src/interface/graphql/resolvers/like/like.mutation.ts
+++ server/src/interface/graphql/resolvers/like/like.mutation.ts
@@ -1,17 +1,17 @@
 import { IContainer } from "@/main/model";
 
-type CreateLikeArgs = {
+type LikeArgs = {
   authorId: string;
   postId: string;
 };
 
 export const likeMutation = {
-  createLike: async (
-    _,
-    args: CreateLikeArgs,
-    { createLikeUseCase }: IContainer,
-  ) => {
+  createLike: async (_, args: LikeArgs, { createLikeUseCase }: IContainer) => {
     const { authorId, postId } = args;
     return createLikeUseCase.execute(authorId, postId);
+  },
+  deleteLike: async (_, args: LikeArgs, { deleteLikeUseCase }: IContainer) => {
+    const { authorId, postId } = args;
+    return deleteLikeUseCase.execute(authorId, postId);
   },
 };
