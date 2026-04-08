44de4b4a33bc4e7446b8f9de6d230a900579997d
michelzaum <michel.oliveira999@hotmail.com>
2026-04-08 08:41:28 -0300

    feat: create 'addPostComment' function in store

--- web/src/pages/postDetails/mutations/createComment.ts
+++ web/src/pages/postDetails/mutations/createComment.ts
@@ -5,7 +5,6 @@ export const CREATE_COMMENT = gql`
     createComment(postId: $postId, content: $content) {
       id
       content
-      authorName
     }
   }
 `;
--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -7,12 +7,19 @@ interface IState {
   feedPostsList: PostCard[];
 }
 
+type PostComment = {
+  id: string;
+  content: string;
+  postId: string;
+};
+
 interface IActions {
   setLoggedUserId: (loggedUserId: string) => void;
   setFeedPostsList: (feedPostsList: PostCard[]) => void;
   addPostLike: (postId: string) => void;
   removePostLike: (postId: string) => void;
   getPostById: (postId: string) => PostCard;
+  addPostComment: (comment: PostComment) => void;
 }
 
 export const useStore = create<IState & IActions>()(
@@ -54,5 +61,24 @@ export const useStore = create<IState & IActions>()(
       })),
     getPostById: (postId: string) =>
       get().feedPostsList.find((post) => post.id === postId),
+    addPostComment: (postComment: PostComment) =>
+      set(({ feedPostsList }) => ({
+        feedPostsList: feedPostsList.map((post) =>
+          post.id === postComment.id
+            ? {
+                ...post,
+                comments: [
+                  ...post.comments,
+                  {
+                    id: postComment.id,
+                    authorId: get().loggedUserId,
+                    content: postComment.content,
+                    postId: postComment.postId,
+                  },
+                ],
+              }
+            : post,
+        ),
+      })),
   })),
 );
