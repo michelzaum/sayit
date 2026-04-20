60223689f6800f5003dbe968f0a7184ad5ec0dad
michelzaum <michel.oliveira999@hotmail.com>
2026-04-19 22:18:13 -0300

    test: add unit test to validate unique user email

--- server/src/modules/user/useCases/createUser/CreateUserUseCase.spec.ts
+++ server/src/modules/user/useCases/createUser/CreateUserUseCase.spec.ts
@@ -26,4 +26,17 @@ describe('CreateUserUseCase', () => {
     expect(newUser.email).toBe(newUserInfo.email);
     expect(inMemoryUserReposiory.users.length).toBe(1);
   });
+
+  it('should not create an user if email is already in use', async () => {
+    const newUserInfo = {
+      email: "johndoe@gmail.com",
+      name: "john",
+      password: "12345678",
+    };
+
+    await createUserUseCase.execute(newUserInfo);
+
+    expect(createUserUseCase.execute(newUserInfo)).rejects.toThrow("E-mail já cadastrado");
+    expect(inMemoryUserReposiory.users.length).toBe(1);
+  });
 });
