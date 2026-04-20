cba837668e6a301c270c87a1c6db9c3df7893f2f
michelzaum <michel.oliveira999@hotmail.com>
2026-04-19 22:24:22 -0300

    test: add Arrange-Act-Assert comments to CreateUserUseCase unit tests

--- server/src/modules/user/useCases/createUser/CreateUserUseCase.spec.ts
+++ server/src/modules/user/useCases/createUser/CreateUserUseCase.spec.ts
@@ -14,14 +14,17 @@ describe('CreateUserUseCase', () => {
   });
 
   it('should create the user correctly', async () => {
+    // Arrange
     const newUserInfo = {
       email: "johndoe@gmail.com",
       name: "john",
       password: "12345678",
     };
 
+    // Act
     const newUser = await createUserUseCase.execute(newUserInfo);
 
+    // Assert
     expect(newUser).toBeTruthy();
     expect(newUser).toHaveProperty('name');
     expect(newUser.email).toBe(newUserInfo.email);
@@ -29,28 +32,34 @@ describe('CreateUserUseCase', () => {
   });
 
   it('should not create an user if email is already in use', async () => {
+    // Arrange
     const newUserInfo = {
       email: "johndoe@gmail.com",
       name: "john",
       password: "12345678",
     };
 
+    // Act
     await createUserUseCase.execute(newUserInfo);
 
+    // Assert
     expect(createUserUseCase.execute(newUserInfo)).rejects.toThrow("E-mail já cadastrado");
     expect(inMemoryUserReposiory.users.length).toBe(1);
   });
 
   it("should correctly hash user's password", async () => {
+    // Arrange
     const newUserInfo = {
       email: "johndoe@gmail.com",
       name: "john",
       password: "12345678",
     };
 
+    // Act
     const newUser = await createUserUseCase.execute(newUserInfo);
     const passwordMatch = await compare("12345678", newUser.password);
 
+    // Assert
     expect(passwordMatch).toBe(true);
     expect(newUser.password).not.toBe(newUserInfo.password);
   });
