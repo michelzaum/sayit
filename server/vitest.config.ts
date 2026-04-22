93d6138c2fde19909f642266ba59d0a577966052
michelzaum <michel.oliveira999@hotmail.com>
2026-04-22 09:28:49 -0300

    chore: add a path alias for unit tests

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vitest/config';
+import path from "node:path";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        coverage: {
-            provider: 'v8',
-        }
-    }
-})
+  test: {
+    coverage: {
+      provider: "v8",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
