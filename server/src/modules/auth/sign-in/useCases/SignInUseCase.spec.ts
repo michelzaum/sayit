6e8f26e08408a87f9ce1baa8d7e7dfa249c5b587
michelzaum <michel.oliveira999@hotmail.com>
2026-04-22 10:03:25 -0300

    test: add unit test to validate if getByEmail is called with correct argument

--- server/src/modules/auth/sign-in/useCases/SignInUseCase.spec.ts
+++ server/src/modules/auth/sign-in/useCases/SignInUseCase.spec.ts
@@ -1,14 +1,17 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { InMemoryUserReposiory } from "@/modules/user/repositories/in-memory/InMemoryUserRepository";
-import { beforeEach, describe, expect, it } from "vitest";
 import { SignInUseCase } from "./SignInUseCase";
+import { CreateUserUseCase } from "@/modules/user/useCases/createUser/CreateUserUseCase";
 
 describe("SignInUseCase", () => {
   let inMemoryUserRepository: InMemoryUserReposiory;
   let signInUseCase: SignInUseCase;
+  let createUserUseCase: CreateUserUseCase;
 
   beforeEach(() => {
     inMemoryUserRepository = new InMemoryUserReposiory();
     signInUseCase = new SignInUseCase(inMemoryUserRepository);
+    createUserUseCase = new CreateUserUseCase(inMemoryUserRepository);
   });
 
   it("should throw an error if email is not found", async () => {
@@ -26,5 +29,23 @@ describe("SignInUseCase", () => {
     await expect(invalidUser).rejects.toThrow(
       `User with e-mail ${userEmail} not found`,
     );
+  });
+
+  it("should call getByEmail with the user's email that is trying to sign-in", async () => {
+    // Arrange
+    const user = {
+      name: "John",
+      email: "john@mail.com",
+      password: "12345678",
+    };
+    const getByEmailSpy = vi.spyOn(inMemoryUserRepository, "getByEmail");
+    const newUser = await createUserUseCase.execute(user);
+
+    // Act
+    const userToSignIn = await signInUseCase.execute(user);
+
+    // Assert
+    expect(getByEmailSpy).toHaveBeenCalledWith(newUser.email);
+    expect(userToSignIn).toBeDefined();
   });
 });
