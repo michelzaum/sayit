b7de1d3c83846e0d145f1d67d1431582baf6ffc7
michelzaum <michel.oliveira999@hotmail.com>
2026-04-23 19:41:40 -0300

    feat: add sign-in page fields schema

--- server/src/modules/auth/sign-in/useCases/SignInUseCase.ts
+++ server/src/modules/auth/sign-in/useCases/SignInUseCase.ts
@@ -1,9 +1,22 @@
 import { compare } from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { z } from "zod";
 
 import { env } from "@/config/env";
 import { IUserRepository } from "@/modules/user/repositories/IUserRepository";
 import { SignInUseCaseInput } from "./model";
+
+const schema = z.object({
+  email: z.email({ error: "E-mail invalido" }),
+  password: z
+    .string()
+    .min(8, {
+      error: "Senha invalida. Minimo 8 caracteres e maximo 16",
+    })
+    .max(16, {
+      error: "Senha invalida. Minimo 8 caracteres e maximo 16",
+    }),
+});
 
 export class SignInUseCase {
   constructor(private readonly userRepository: IUserRepository) {}
