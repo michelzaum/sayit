c72755a1d32e90db7c54711bebc4dccfded255cb
michelzaum <michel.oliveira999@hotmail.com>
2026-04-26 11:38:37 -0300

    test: add unit tests for useCreatePost hook validation logic

---
+++ web/src/components/create-post/create-post.spec.tsx
@@ -0,0 +1,40 @@
+import { describe, expect, it, vi } from 'vitest';
+import { renderHook } from '@testing-library/react';
+import { useCreatePost } from './useCreatePost';
+import { MockedProvider } from '@apollo/client/testing/react';
+import { MemoryRouter } from 'react-router';
+
+const mockCreatePost = vi.fn();
+
+vi.mock("@apollo/client/react", async () => {
+  const actual = (await vi.importActual("@apollo/client/react")) as object;
+  return {
+    ...actual,
+    useMutation: () => [mockCreatePost, {}],
+  };
+});
+
+describe('useCreatePost', () => {
+  const event = {
+    preventDefault: vi.fn(),
+  } as unknown as React.FormEvent<HTMLFormElement>;
+
+  const { result } = renderHook(() => useCreatePost(), {
+    wrapper: ({ children }) => (
+      <MockedProvider>
+        <MemoryRouter>{children}</MemoryRouter>
+      </MockedProvider>
+    )
+  });
+
+  it('should not call createPost if postContent is empty', async () => {
+    // Arrange
+    result.current.postContentRef.current.value = '';
+
+    // Act
+    await result.current.onCreatePostSubmit(event);
+
+    // Assert
+    expect(mockCreatePost).not.toHaveBeenCalled();
+  });
+});
