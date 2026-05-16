3e1d1dbbe49c75db4901757f54db0ce24f85aff9
michelzaum <michel.oliveira999@hotmail.com>
2026-05-16 19:16:09 -0300

    feat: add toast feedback to sign-out process

--- web/src/components/header/useHeader.ts
+++ web/src/components/header/useHeader.ts
@@ -1,5 +1,6 @@
-import { useMutation } from "@apollo/client/react";
 import { useNavigate } from 'react-router';
+import { toast } from "sonner";
+import { useMutation } from "@apollo/client/react";
 import { SIGN_OUT } from "./mutation";
 
 export function useHeader() {
@@ -10,8 +11,10 @@ export function useHeader() {
     try {
       await signOut();
       navigate('/sign-in');
+      toast.success("Sessão encerrada com sucesso!");
     } catch {
       console.log('error');
+      toast.error("Erro ao encerrar sessão. Tente novamente")
     }
   }
 
