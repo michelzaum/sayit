43433e9bea2f7560efd94441c0ff015a0e6522f6
michelzaum <michel.oliveira999@hotmail.com>
2026-05-17 21:10:53 -0300

    feat: update GetUserUseCase class to get userId from accessToken in request

--- server/src/modules/user/useCases/getUser/GetUserUseCase.ts
+++ server/src/modules/user/useCases/getUser/GetUserUseCase.ts
@@ -1,9 +1,34 @@
+import { IncomingMessage } from "http";
+import jwt from "jsonwebtoken";
+
 import { IUserRepository } from "../../repositories/IUserRepository";
+import { env } from "@/config/env";
 
 export class GetUserUseCase {
-  constructor(private readonly userRepository: IUserRepository) {}
+  constructor(private readonly userRepository: IUserRepository) { }
 
-  async execute(id: string) {
-    return this.userRepository.getById(id);
+  async execute(request: IncomingMessage) {
+    const cookie = request.headers.cookie;
+
+    if (!cookie) {
+      throw new Error("No cookies found");
+    }
+
+    if (!cookie.includes("accessToken")) {
+      throw new Error("access token not found");
+    }
+
+    const [_, accessToken] = cookie.split("=");
+
+    const { sub: userId } = jwt.verify(
+      accessToken,
+      env.jwtSecret,
+    ) as jwt.JwtPayload;
+
+    if (!userId) {
+      throw new Error("Invalid access token. No 'sub' value found.");
+    }
+
+    return this.userRepository.getById(userId);
   }
 }
