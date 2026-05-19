1d66073e1da19728d6d017ec8425160096c24de0
michelzaum <michel.oliveira999@hotmail.com>
2026-05-18 22:41:51 -0300

    feat: create GET_ALL_POSTS_BY_AUTHOR_ID query

---
+++ web/src/pages/profile/queries/getAllPostsByAuthorId.ts
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const GET_ALL_POSTS_BY_AUTHOR_ID = gql`
+  query GetAllPostsByAuthorId($authorId: String) {
+    getAllPostsByAuthorId(authorId: $authorId) {
+      content
+      createdAt
+      comments {
+        id
+        author {
+          id
+          name
+        }
+        content
+      }
+      likes {
+        postId
+        authorId
+      }
+    }
+  }
+`;
