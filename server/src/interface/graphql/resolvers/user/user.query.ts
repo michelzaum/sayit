94ec562862af9d4a46aabe5bb5111b76a74205ac
michelzaum <michel.oliveira999@hotmail.com>
2026-05-21 09:16:12 -0300

    feat: create getUserProfileInfo query resolver

--- server/src/interface/graphql/resolvers/user/user.query.ts
+++ server/src/interface/graphql/resolvers/user/user.query.ts
@@ -1,8 +1,18 @@
 import { IContainer } from "@/main/model";
 
+type GetUserProfileInfoArgs = {
+  id: string;
+}
+
 export const userQuery = {
   getLoggedUser: async (_, __, { http, getUserUseCase }: IContainer) => {
     const { req } = http;
     return await getUserUseCase.execute(req);
+  },
+  getUserProfileInfo: async (_, args: GetUserProfileInfoArgs, { http, getUserProfileInfoUseCase }: IContainer) => {
+    const { id } = args;
+    const { req } = http;
+
+    return getUserProfileInfoUseCase.execute(req, id);
   },
 };
