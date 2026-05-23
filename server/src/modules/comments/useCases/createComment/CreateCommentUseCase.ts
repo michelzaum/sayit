c85b3d3654285228b8a1393e4d3c0c328ec5e1cc
michelzaum <michel.oliveira999@hotmail.com>
2026-05-23 19:34:23 -0300

    refactor: decouple authentication from CreateCommentUseCase by passing authorId directly

--- server/src/modules/comments/useCases/createComment/CreateCommentUseCase.ts
+++ server/src/modules/comments/useCases/createComment/CreateCommentUseCase.ts
@@ -1,34 +1,9 @@
-import { IncomingMessage } from "http";
-import jwt from "jsonwebtoken";
-
-import { env } from "@/config/env";
 import { ICommentRepository } from "../../repositories/ICommentRepository";
 
 export class CreateCommentUseCase {
-  constructor(private readonly commentRepository: ICommentRepository) {}
+  constructor(private readonly commentRepository: ICommentRepository) { }
 
-  async execute(postId: string, content: string, request: IncomingMessage) {
-    const cookie = request.headers.cookie;
-
-    if (!cookie) {
-      throw new Error("No cookies found");
-    }
-
-    if (!cookie.includes("accessToken")) {
-      throw new Error("access token not found");
-    }
-
-    const [_, accessToken] = cookie.split("=");
-
-    const { sub: authorId } = jwt.verify(
-      accessToken,
-      env.jwtSecret,
-    ) as jwt.JwtPayload;
-
-    if (!authorId) {
-      throw new Error("Invalid access token. No 'sub' value found.");
-    }
-
+  async execute(postId: string, content: string, authorId: string) {
     return this.commentRepository.create(authorId, postId, content);
   }
 }
