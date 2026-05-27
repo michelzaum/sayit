07bf904f7f35d9065a716f6f14f510f588f5ac41
michelzaum <michel.oliveira999@hotmail.com>
2026-05-27 09:23:25 -0300

    feat: create updateUser mutation

--- server/src/interface/graphql/resolvers/user/user.mutation.ts
+++ server/src/interface/graphql/resolvers/user/user.mutation.ts
@@ -1,3 +1,5 @@
+import { IContainer } from "@/main/model";
+
 interface CreateUserArgs {
   body: {
     email: string;
@@ -6,8 +8,21 @@ interface CreateUserArgs {
   };
 }
 
+interface UpdateUserArgs {
+  id: string;
+  body: {
+    name: string;
+    bio: string;
+    password: string;
+  }
+}
+
 export const userMutation = {
-  createUser: async (_, args: CreateUserArgs, { createUserUseCase }) => {
+  createUser: async (_, args: CreateUserArgs, { createUserUseCase }: IContainer) => {
     return await createUserUseCase.execute(args.body);
+  },
+  updateUser: async (_, args: UpdateUserArgs, { updateUserUseCase }: IContainer) => {
+    const { id, body } = args;
+    return updateUserUseCase.execute(id, body);
   },
 };
