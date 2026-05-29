af50730736ac7e7cf4e7e1796ba98b21718c6d46
michelzaum <michel.oliveira999@hotmail.com>
2026-05-28 21:22:02 -0300

    refactor: rename UserInput to CreateUserInput in CREATE_USER mutation

--- web/src/pages/sign-up/mutation.ts
+++ web/src/pages/sign-up/mutation.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_USER = gql`
-  mutation CreateUser($body: UserInput!) {
+  mutation CreateUser($body: CreateUserInput!) {
     createUser(body: $body) {
       user {
         email
