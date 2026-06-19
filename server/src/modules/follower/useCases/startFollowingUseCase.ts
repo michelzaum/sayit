5332b0eaef82f5845a39ce5ade77d4ca7c333dd5
michelzaum <michel.oliveira999@hotmail.com>
2026-06-19 09:03:36 -0300

    feat: add validation in StartFollowingUseCase.execute method to prevent user following itself

--- server/src/modules/follower/useCases/startFollowingUseCase.ts
+++ server/src/modules/follower/useCases/startFollowingUseCase.ts
@@ -4,6 +4,10 @@ export class StartFollowingUseCase {
   constructor(private readonly followerRepository: IFollowerRepository) {}
 
   async execute(userFollowedId: string, followedByUserId: string) {
+    if (userFollowedId === followedByUserId) {
+      throw new Error("User can't follow itself");
+    }
+
     return this.followerRepository.startFollow(
       userFollowedId,
       followedByUserId,
