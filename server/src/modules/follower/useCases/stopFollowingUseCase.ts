d349ca5198835315d4c2a1c133f95c9910358391
michelzaum <michel.oliveira999@hotmail.com>
2026-06-20 19:29:35 -0300

    feat: add validation to prevent user to unfollow itself

--- server/src/modules/follower/useCases/stopFollowingUseCase.ts
+++ server/src/modules/follower/useCases/stopFollowingUseCase.ts
@@ -4,6 +4,10 @@ export class StopFollowingUseCase {
   constructor(private readonly followeRepository: IFollowerRepository) {}
 
   async execute(userFollowedId: string, followedByUserId: string) {
+    if (userFollowedId === followedByUserId) {
+      throw new Error("User can't unfollow itself");
+    }
+
     return this.followeRepository.stopFollow(userFollowedId, followedByUserId);
   }
 }
