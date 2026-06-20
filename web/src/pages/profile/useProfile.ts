2c9a2ec342c18364591020fe6a163785910501cf
michelzaum <michel.oliveira999@hotmail.com>
2026-06-20 19:52:51 -0300

    refactor: update useEffect array dependencies

--- web/src/pages/profile/useProfile.ts
+++ web/src/pages/profile/useProfile.ts
@@ -3,32 +3,48 @@ import { useParams } from "react-router";
 import { useLazyQuery } from "@apollo/client/react";
 
 import { months } from "@/shared/constants/months";
-import { GetAllPostsByAuthorId, GetUserProfileInfo, UserProfileInfo } from "./types";
+import {
+  GetAllPostsByAuthorId,
+  GetUserProfileInfo,
+  UserProfileInfo,
+} from "./types";
 import { GET_ALL_POSTS_BY_AUTHOR_ID } from "./queries/getAllPostsByAuthorId";
 import { GET_USER_PROFILE_INFO } from "./queries/getUserProfileInfo";
 
 export function useProfile() {
   const { id } = useParams<{ id: string }>();
-  const [getUserProfileInfo] = useLazyQuery<GetUserProfileInfo>(GET_USER_PROFILE_INFO, { fetchPolicy: 'no-cache' });
-  const [getAllPostsByAuthorId] = useLazyQuery<GetAllPostsByAuthorId>(GET_ALL_POSTS_BY_AUTHOR_ID, { fetchPolicy: 'no-cache' });
-  const [userInfo, setUserInfo] = useState<UserProfileInfo>({} as UserProfileInfo);
-  const [userPostsInfo, setUserPostsInfo] = useState<GetAllPostsByAuthorId>({} as GetAllPostsByAuthorId);
+  const [getUserProfileInfo] = useLazyQuery<GetUserProfileInfo>(
+    GET_USER_PROFILE_INFO,
+    { fetchPolicy: "no-cache" },
+  );
+  const [getAllPostsByAuthorId] = useLazyQuery<GetAllPostsByAuthorId>(
+    GET_ALL_POSTS_BY_AUTHOR_ID,
+    { fetchPolicy: "no-cache" },
+  );
+  const [userInfo, setUserInfo] = useState<UserProfileInfo>(
+    {} as UserProfileInfo,
+  );
+  const [userPostsInfo, setUserPostsInfo] = useState<GetAllPostsByAuthorId>(
+    {} as GetAllPostsByAuthorId,
+  );
   const [isLoggedUserFollowing, setIsLoggedUserFollowing] = useState(false);
   const [isUnFollowUserModalOpen, setIsUnFollowUserModalOpen] = useState(false);
 
   useEffect(() => {
     async function handleGetUserInfoAndPosts() {
-      const [
-        { data: userProfileInfo },
-        { data: allPostsByAuthorId },
-      ] = await Promise.all([
-        getUserProfileInfo({ variables: { userId: id } }),
-        getAllPostsByAuthorId({ variables: { authorId: id } }),
-      ]);
+      const [{ data: userProfileInfo }, { data: allPostsByAuthorId }] =
+        await Promise.all([
+          getUserProfileInfo({ variables: { userId: id } }),
+          getAllPostsByAuthorId({ variables: { authorId: id } }),
+        ]);
 
       if (userProfileInfo.getUserProfileInfo) {
-        const userCreatedAtMonth = new Date(Number(userProfileInfo.getUserProfileInfo.userInfo.createdAt)).getMonth();
-        const userCreatedAtYear = new Date(Number(userProfileInfo.getUserProfileInfo.userInfo.createdAt)).getFullYear();
+        const userCreatedAtMonth = new Date(
+          Number(userProfileInfo.getUserProfileInfo.userInfo.createdAt),
+        ).getMonth();
+        const userCreatedAtYear = new Date(
+          Number(userProfileInfo.getUserProfileInfo.userInfo.createdAt),
+        ).getFullYear();
 
         setUserInfo(() => ({
           userInfo: {
@@ -46,15 +62,15 @@ export function useProfile() {
     }
 
     handleGetUserInfoAndPosts();
-  }, [id]);
+  }, [getAllPostsByAuthorId, getUserProfileInfo, id]);
 
   function handleFollowUser() {
-    console.log('followed!');
+    console.log("followed!");
     setIsLoggedUserFollowing(true);
   }
 
   function handleUnFollowUser() {
-    console.log('not following anymore...');
+    console.log("not following anymore...");
     setIsLoggedUserFollowing(false);
     closeUnFollowUserModal();
   }
@@ -77,5 +93,5 @@ export function useProfile() {
     handleUnFollowUser,
     handleFollowUser,
     openUnFollowUserModal,
-  }
+  };
 }
