11ad98fc22e84becf8d4761833c2f08082fab9b6
michelzaum <michel.oliveira999@hotmail.com>
2026-06-20 19:30:19 -0300

    refactor: rename followerRepository argument

--- server/src/modules/follower/useCases/stopFollowingUseCase.ts
+++ server/src/modules/follower/useCases/stopFollowingUseCase.ts
@@ -1,13 +1,13 @@
 import { IFollowerRepository } from "../repositories/IFollowerRepository";
 
 export class StopFollowingUseCase {
-  constructor(private readonly followeRepository: IFollowerRepository) {}
+  constructor(private readonly followerRepository: IFollowerRepository) {}
 
   async execute(userFollowedId: string, followedByUserId: string) {
     if (userFollowedId === followedByUserId) {
       throw new Error("User can't unfollow itself");
     }
 
-    return this.followeRepository.stopFollow(userFollowedId, followedByUserId);
+    return this.followerRepository.stopFollow(userFollowedId, followedByUserId);
   }
 }
