9f6b715d1f10a8558bb9a9760a6ddbd30767141f
michelzaum <michel.oliveira999@hotmail.com>
2026-07-07 21:48:25 -0300

    chore: update server and client config for prod env

--- server/src/main/server.ts
+++ server/src/main/server.ts
@@ -97,7 +97,7 @@ app.use(
 );
 
 await new Promise<void>((resolve) =>
-  httpServer.listen({ port: 4000 }, resolve),
+  httpServer.listen({ port: process.env.PORT || 4000 }, resolve),
 );
 
-console.log("Server ready at http://localhost:4000");
+console.log(`Server ready at http://localhost:${process.env.PORT || 4000}`);
